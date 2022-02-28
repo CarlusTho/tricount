b20afd1ade14c78cc7b2de45160b9b1ff16c0b64
tcar <thomasulrac@gmail.com>
2022-02-28 23:36:28 +0100

    Corrections

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 console.log("Salut");
+
+class Participant {
+    public bonjour() {
+        return this.nom + this.prenom;
+    }
+    constructor(public nom: string, public prenom: string) {}
+}
+
+class Transaction {
+    constructor(public parti: string, public label: string, public montant: number, public transaction: number) {}
+}
+
+
+let tabParticipant:string[];
+let tabTransaction = [];
+
+tabParticipant.push()
